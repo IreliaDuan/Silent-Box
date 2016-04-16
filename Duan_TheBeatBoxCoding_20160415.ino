e7a66ba15d84d344c42b453fd874dce92422dcc0
IreliaDuan <ireliadyx@gmail.com>
2016-04-16 15:29:19 -0700

    Added files via upload

---
+++ Duan_TheBeatBoxCoding_20160415.ino
@@ -0,0 +1,49 @@
+【Original authors:Haohua Zhang】
+【Addition & revision: Irelia Duan】
+
+int sensorPin = A0; 
+int ledPin = 11;
+int sensorValue = 0;
+int balance = 0;
+int countZero = 0;
+int holding = 0;
+int delayTime = 50;
+
+void setup () 
+{
+  pinMode (ledPin, OUTPUT);
+  Serial.begin (9600);
+}
+ 
+void loop (){
+  ReadVolume();
+  ShowLED();
+  
+  Serial.println (balance, DEC);
+  delay(delayTime);
+}
+
+void ShowLED(){
+  int ld = balance / 4;
+  ld = 255 - ld * 200;
+  ld = ld < 0 ? 0 : ld;
+  analogWrite(ledPin, ld);
+}
+
+void ReadVolume(){ 
+  sensorValue = analogRead (sensorPin);
+  if(sensorValue == 0){
+    countZero++;
+    if(countZero >= (holding * 400 / delayTime)){
+      balance = 0;
+    }
+  }else{
+    countZero = 0;
+    if(balance == 0){
+      balance = sensorValue;
+    }else{
+      balance = (balance * 2 + sensorValue) / 3;
+    }
+  }
+}
+
